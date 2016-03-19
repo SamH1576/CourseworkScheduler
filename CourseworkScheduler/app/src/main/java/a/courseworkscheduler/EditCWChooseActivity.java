fea72b91ad9c85ba71ef3459debe98f324babc46
Sam H <sam.hiscox@ntlworld.com>
2016-03-19 23:40:30 +0000

    All Java Comments Added
    Notes for all Java has been added

--- CourseworkScheduler/app/src/main/java/a/courseworkscheduler/EditCWChooseActivity.java
+++ CourseworkScheduler/app/src/main/java/a/courseworkscheduler/EditCWChooseActivity.java
@@ -29,73 +29,72 @@ public class EditCWChooseActivity extends AppCompatActivity { //Code for the Edi
         setContentView(R.layout.activity_edit_cwchoose); //sets the activity layout to be the Edit_CWchoose xml
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() { //handles an item in the array list being clicked
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) { //when an item is clickd...
-                TextView item =(TextView) view;
-                String message = item.getText().toString();
-                sendtoAddCW(message); //...TODO cont
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) { //when an item is clicked...
+                TextView item =(TextView) view; //identify the item clicked ...
+                String message = item.getText().toString(); //fetch its text
+                sendtoAddCW(message); //call to method sendtoAddCW with the CWName that was clicked
             }
         };
-        ListView lv = (ListView) findViewById(R.id.listView);
-        lv.setOnItemClickListener(itemClickListener);
+        ListView lv = (ListView) findViewById(R.id.listView); //declare and initialise a listView object
+        lv.setOnItemClickListener(itemClickListener); //activates the OnItemClickListener prepared above
 
-        List<String> your_array_list = new ArrayList<String>();
-        String data = returnCWData();
-        stringtoarray s2a = new stringtoarray();
-        s2a.MatrixMaker(data);
-        int arraylength = stringtoarray.finalmatrix.length;
-        for (int i = 0; i < arraylength; i++) {
-            String tempdata = (finalmatrix[i][0]);
-            if (tempdata != null) {
-                your_array_list.add(tempdata);
+        List<String> your_array_list = new ArrayList<String>(); //declare a new arraylist of strings
+        String data = returnCWData(); //fetch the CW Data from the storage file
+        stringtoarray s2a = new stringtoarray(); //declare a new instance of stringtoarray
+        s2a.MatrixMaker(data); //calls the MatrixMaker method of stringtoarray, which uses the CW data pulled from the storage file and converts it to a matrix
+        int arraylength = stringtoarray.finalmatrix.length; //determine the length (no of rows) of the matrix
+        for (int i = 0; i < arraylength; i++) { //for each row...
+            String tempdata = (finalmatrix[i][0]); //grab the first element...
+            if (tempdata != null) { //check it contains data..
+                your_array_list.add(tempdata); //if it does, add it to the array list being built
             }
         }
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>( //Using the arraylist of string, initialise an arrayAdapter which can be used to move the data into a ListView
                 this,
                 android.R.layout.simple_list_item_1,
                 your_array_list);
 
-        lv.setAdapter(arrayAdapter);
-        Log.d("Log", "Complete");
+        lv.setAdapter(arrayAdapter); //finally, fill the listView (list of items on display) with the data from arrayAdapter
+        Log.d("Log", "Complete"); //for debugging purposes only
 
-        if(!arrayAdapter.getItem(0).isEmpty()){
-            Log.d("Log", "Nothing to show");
-
-            return;
+        if(!arrayAdapter.getItem(0).isEmpty()){ //check the first element of the array is not empty
+            Log.d("Log", "Nothing to show"); //(for debugging)
+            return; //if not empty, then there is data to display, simply return to exit the onCreate method
         }
-        Toast.makeText(this, "Nothing to show!",
+        Toast.makeText(this, "Nothing to show!", //if there is no data in the ListView, display a notification
                 Toast.LENGTH_LONG).show();
 
     }
 
-    public String returnCWData() {
-        BufferedReader bufferReader = null;
-        StringBuilder result = new StringBuilder();
-        try {
-            FileInputStream fileInputStream = openFileInput("CWStore");
-            bufferReader = new BufferedReader(new InputStreamReader(fileInputStream));
+    public String returnCWData() { //Returns a string of the data which is currently stored NB Added to each class as cannot be static due to openFileStream
+        BufferedReader bufferReader = null; //Initialise a buffer reader, which will read the file
+        StringBuilder result = new StringBuilder(); //Initialise a stringbuilder, which puts together strings
+        try { //try, catch structure used to handle errors
+            FileInputStream fileInputStream = openFileInput("CWStore"); //Initialise an input stream and target file, required for the reader below
+            bufferReader = new BufferedReader(new InputStreamReader(fileInputStream)); //Point the reader at the target file
             String line;
-            while ((line = bufferReader.readLine()) != null) {
-                result.append(line);
+            while ((line = bufferReader.readLine()) != null) { //Whilst there are sill lines to be read...
+                result.append(line); //add that line to the string
             }
-        } catch (IOException e) {
+        } catch (IOException e) { //error handling
             e.printStackTrace();
-        } finally {
+        } finally { //assuming there are no errors in the try catch above...
             try {
-                assert bufferReader != null;
-                bufferReader.close();
+                assert bufferReader != null; //checking the file isn't null, ie not opened
+                bufferReader.close(); //close the file
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        return String.valueOf(result); //return the file data as a string
+    }
 
-        return String.valueOf(result);
-    } //Added to each class as cannot be static due to openFileStream
-    public void sendtoAddCW(String message){
-        Intent intent = new Intent(this, AddCoursework.class);
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-        finish();
+    public void sendtoAddCW(String message){ //method to start AddCoursework activity, passing an intent with CWName as the message
+        Intent intent = new Intent(this, AddCoursework.class); //create a new intent and point it to AddCoursework
+        intent.putExtra(EXTRA_MESSAGE, message); //add the message to the intent
+        startActivity(intent); //start the AddCoursework activity
+        finish(); //Ensures on completion of AddCoursework the app does not return to the edit choose CW activity
     }
 
 
--- CourseworkScheduler/app/src/main/java/a/courseworkscheduler/ViewCWListActivity.java
+++ CourseworkScheduler/app/src/main/java/a/courseworkscheduler/ViewCWListActivity.java
@@ -11,31 +11,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ViewCWListActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "a.courseworkscheduler.MESSAGE";
+    public final static String EXTRA_MESSAGE = "a.courseworkscheduler.MESSAGE"; //declares a variable which can be used to transfer messages using intents
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //code executed on creation of the activity
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_cwlist);
+        setContentView(R.layout.activity_view_cwlist); //sets the xml content to display
     }
 
-    public void onClickOrderName(View view){
-        Intent intent = new Intent(this, ViewOrderedCWListActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, "Name");
-        startActivity(intent);
-
-    }
-
-    public void onClickOrderDate(View view){
-        Intent intent = new Intent(this, ViewOrderedCWListActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, "Date");
-        startActivity(intent);
+    public void onClickOrderName(View view){ //handles the Order by Name button
+        Intent intent = new Intent(this, ViewOrderedCWListActivity.class); //create a new intent and point it at the ViewOrderedCWList activity
+        intent.putExtra(EXTRA_MESSAGE, "Name"); //add the "Name" string as a message in the intent, used to differentiate the calls to ViewOrderedCWList
+        startActivity(intent); //start the new activity
 
     }
 
-    public void onClickOrderWeight(View view){
-        Intent intent = new Intent(this, ViewOrderedCWListActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, "Weight");
-        startActivity(intent);
+    public void onClickOrderDate(View view){ //handles the Order by Date button
+        Intent intent = new Intent(this, ViewOrderedCWListActivity.class); //create a new intent and point it at the ViewOrderedCWList activity
+        intent.putExtra(EXTRA_MESSAGE, "Date"); //add the "Date" string as a message in the intent, used to differentiate the calls to ViewOrderedCWList
+        startActivity(intent); //start the new activity
+
+    }
+
+    public void onClickOrderWeight(View view){ //handles the Order by Weighting button
+        Intent intent = new Intent(this, ViewOrderedCWListActivity.class); //create a new intent and point it at the ViewOrderedCWList activity
+        intent.putExtra(EXTRA_MESSAGE, "Weight"); //add the "Weight" string as a message in the intent, used to differentiate the calls to ViewOrderedCWList
+        startActivity(intent); //start the new activity
 
     }
 }
